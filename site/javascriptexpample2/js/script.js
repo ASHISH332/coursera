cc8c1cf0c6696714fcf678aff2d28d9d6cbe3d0b
Ashish332 <ashishkf007@gmail.com>
2020-06-04 18:05:42 +0530

    new update

---
+++ site/javascriptexpample2/js/script.js
@@ -0,0 +1,30 @@
+// console.log(document.getElementById("title"));
+// console.log();
+function sayHello() {
+	var name=
+	
+		document.getElementById("name").value;
+	var message = "<h2>hello"+ name + "!</h2>";
+
+	// document
+	// .getElementById("content")
+	// .textContent=message;
+	document
+	.getElementById("content")
+	.innerHTML=message;
+
+if (name ==="ash"){
+	var tite =
+	document
+	.querySelector("#tite")
+	.textContent;
+	title+="kumar";
+
+	document
+	.querySelector("h1")
+	.textContent;
+	title+=title;
+}
+
+
+}
